5f0d1b546435ee58ef521627908f6e26ba111502
Tushar Pandey <tpandey1006@gmail.com>
2025-07-10 02:23:27 +0530

    Fix CORS origin for frontend

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:
+  "https://expense-tracker-frontend-g3p1.onrender.com"
+}));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI)
